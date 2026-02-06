9b1684488293c8595acbbc2f9df2b88a426233c9
almirAlmir <aasc@cin.ufpe.br>
2026-02-05 22:55:21 -0300

    fix(login): Adicionando tipo ao Record

--- usuario-service/src/main/java/com/apirest/usuario/business/dto/UsuarioRecord.java
+++ usuario-service/src/main/java/com/apirest/usuario/business/dto/UsuarioRecord.java
@@ -1,4 +1,4 @@
 package com.apirest.usuario.business.dto;
 
-public record UsuarioRecord(String nome, String email, Long id) {
+public record UsuarioRecord(String nome, String email, Long id, String tipo) {
 }
